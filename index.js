ce8e871f2dbaa7c75dbac4f69d3ae983bcf2eda4
nirmal <nirmal@flairlabs.com>
2020-08-16 01:48:02 +0530

    deploy

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 const express = require("express");
 const socket = require('socket.io');
 const { static } = require('express');
+const { Socket } = require('socket.io-client');
 let port = process.env.PORT || 5000;
-// let socketsArray = [];
+let socketsArray = [];
 const app = express();
 
 app.use(express.static('public'));
@@ -19,27 +20,39 @@ const server = app.listen(port, () =>{
 const io = socket(server);
 
 io.on('connection', (socket) => {
+    socketsArray.push(socket.id);
+
+    io.to(socket.id).emit('get-users', {
+        users: socketsArray.filter(id => id != socket.id)
+    });
+
     socket.broadcast.emit('add-user', {
         userId: socket.id
     });
 
     socket.on('disconnect', () => {
-        socketsArray.splice(socketsArray.indexOf(socket.id), 1);
+        socketsArray = socketsArray.splice( socketsArray.indexOf(socket.id) , 1);
         io.emit('remove-user', socket.id);
     });
 
-    socket.on('make-offer', (data) => {
+    socket.on('make-offer', data => {
         socket.to(data.to).emit('offer-made', {
             offer: data.offer,
-            socket: socket.id
+            id: socket.id
         });
     });
 
     socket.on('make-answer', (data) => {
         socket.to(data.to).emit('answer-made', {
-            socket: socket.id,
+            id: socket.id,
             answer: data.answer
         });
     });
+
+    socket.on('ice-candidate', data => {
+        socket.to(data.to).emit('new-ice-candidate', {
+            candidate:data.candidate
+        })
+    })
 
 });
--- public/index.js
+++ public/index.js
@@ -1,18 +1,9 @@
-// const io = require('socket.io-client');
-// const socket = io.Socket('http://localhost:5000');
-
-// const configuration = {
-//     iceServers: [{
-//         url: "stun:stun.services.mozilla.com"
-//     }]
-// }
-
-// let pc = new RTCPeerConnection(configuration);
-
-
-
-
 var socket = io.connect(window.location.origin); 
+
+let userIdsDiv = document.querySelector('#userId');
+
+let remoteSocket = '';
+
 const configuration = {
     iceServers: [
       {
@@ -23,97 +14,200 @@ const configuration = {
       },
     ]
 };
-var answersFrom = {}, offer;
+
 let pc = new RTCPeerConnection(configuration);
 
-function setLocalMediaStream(){
-    navigator.mediaDevices.getUserMedia({video: true, audio: true}).then( stream => {
-        const localVideo = document.querySelector('#video-local');
-        localVideo.srcObject = stream;
-        stream.getTracks().forEach(track => {
-            pc.addTrack(track, stream);
-        });
+pc.addEventListener('icecandidate', handleIcecandidate);
+pc.addEventListener('track', handleRemoteStream);
+
+socket.on('add-user', handleAddUser);
+socket.on('get-users', handleGetUsers);
+socket.on('remove-user', handleRemoveUser);
+socket.on('offer-made', handleOffer);
+socket.on('answer-made', handleAnswer);
+socket.on('new-ice-candidate', handleNewIcecandidate);
+
+
+function handleAddUser(data){
+    let node = document.createElement('li');
+    node.setAttribute('id', data.userId);
+    node.innerText = data.userId;
+    userIdsDiv.appendChild(node);
+    node.addEventListener('click', () => {
+        createOffer(data.userId);
     })
 }
 
-
-
-
-// var pc = new peerConnection({
-//     iceServers: [{
-//         url:["stun:stun.services.mozilla.com",
-//             'stun:stun1.l.google.com:19302',
-//             'stun:stun2.l.google.com:19302',]
-//     }]
-// });
-
-// pc. = function (obj) {
-//     var vid = document.createElement('video');
-//     vid.setAttribute('class', 'video-small');
-//     vid.setAttribute('autoplay', 'autoplay');
-//     vid.setAttribute('id', 'video-small');
-//     document.getElementById('users-container').appendChild(vid);
-//     vid.srcObject = obj.stream;
-// }
-
-let remoteStream = new MediaStream();
-
-
-pc.addEventListener('track', async (event) => {
-    let remoteStr = remoteStream.addTrack(event.track, remoteStream);
-    const remoteVideo = document.querySelector('#video-remote');
-    remoteVideo.srcObject = remoteStr;
-});
-
-// navigator.getUserMedia({video: true, audio: true}, function (stream) {
-//     var video = document.querySelector('video');
-//     video.srcObject = stream;
-//     pc.addStream(stream);
-// }, error);
-
-
-socket.on('add-users', function (data) {
-    let userIdDiv = document.querySelector("#userId");
-    userIdDiv.appendChild(
-        document.createElement('ul').innerHTML = data.userId
-    );
-    let idBtn = document.querySelector('button');
-    idBtn.addEventListener('click', (e)=>{
-        let id = idBtn.innerHTML;
-        createOffer(id);
+function handleGetUsers(data){
+    // console.log(data);
+    data.users.forEach(id => {
+        let node = document.createElement('li');
+        node.setAttribute('id', id);
+        node.innerText = id;
+        userIdsDiv.appendChild(node);
     })
-    // createOffer(data.userId);
-});
+}
 
-socket.on('remove-user', function (id) {
-    
-});
+function handleRemoveUser(id){
+    let idNode = document.getElementById(id);
+    userIdsDiv.removeChild(idNode);
+}
 
-
-socket.on('offer-made', async (data) => {
-    await pc.setRemoteDescription(data.offer);
-    let answer = await pc.createAnswer();
-    setLocalMediaStream();
-    socket.emit('make-answer', {
-        answer:answer,
-        to:data.id
-    });
-});
-
-socket.on('answer-made', async (data) => {
-    await pc.setRemoteDescription(data.answer);
-    setLocalMediaStream();
-});
-
-async function createOffer(id) {
+async function createOffer(id){
+    remoteSocket = id;
     let offer = await pc.createOffer();
     await pc.setLocalDescription(offer);
-    socket.emit('make-offer', {
+    let payload = {
         offer:offer,
         to:id
+    }
+    socket.emit('make-offer', payload);
+    addTrack();
+}
+
+async function handleOffer(data){
+    let offer = new RTCSessionDescription(data.offer)
+    await pc.setRemoteDescription(offer);
+    let answer = await pc.createAnswer();
+    await pc.setLocalDescription(answer);
+    let payload = {
+        answer:answer,
+        to:data.id
+    }
+    socket.emit('make-answer', payload);
+    addTrack();
+}
+
+async function handleAnswer(data){
+    let answer = new RTCSessionDescription(data.answer);
+    await pc.setRemoteDescription(answer);
+}
+
+function addTrack(){
+    navigator.mediaDevices.getUserMedia({video: true, audio:true}).then(stream => {
+        let localStream = stream;
+        const localVideo = document.querySelector('#video-local');
+        localVideo.srcObject = localStream;
+        localStream.getTracks().forEach(track => {
+            pc.addTrack(track, localStream);
+        })
     })
 }
 
-function error(err) {
-    console.warn('Error', err);
+function handleRemoteStream(ev){
+    console.log(ev.streams);
+    let remoteVideo = document.querySelector('#video-remote');
+    remoteVideo.srcObject = ev.streams[0];
 }
+
+function handleIcecandidate(ev){
+    if(ev.candidate){
+        let payload = {
+            candidate:ev.candidate,
+            to:remoteSocket
+        }
+        socket.emit('ice-candidate', payload);
+    }
+}
+
+function handleNewIcecandidate(data){
+    let candidate = new RTCIceCandidate(data.candidate);
+    pc.addIceCandidate(candidate).then( res => {
+        console.log("candidate added" + res);
+    }).catch(e => {
+        console.log(e);
+    })
+}
+
+
+// function setLocalMediaStream(){
+//     navigator.mediaDevices.getUserMedia({video: true, audio: true}).then( stream => {
+//         const localVideo = document.querySelector('#video-local');
+//         localVideo.srcObject = stream;
+//         stream.getTracks().forEach(track => {
+//             pc.addTrack(track, stream);
+//         });
+//     })
+// }
+
+
+// setLocalMediaStream();
+
+// // var pc = new peerConnection({
+// //     iceServers: [{
+// //         url:["stun:stun.services.mozilla.com",
+// //             'stun:stun1.l.google.com:19302',
+// //             'stun:stun2.l.google.com:19302',]
+// //     }]
+// // });
+
+// // pc. = function (obj) {
+// //     var vid = document.createElement('video');
+// //     vid.setAttribute('class', 'video-small');
+// //     vid.setAttribute('autoplay', 'autoplay');
+// //     vid.setAttribute('id', 'video-small');
+// //     document.getElementById('users-container').appendChild(vid);
+// //     vid.srcObject = obj.stream;
+// // }
+
+// let remoteStream = new MediaStream();
+
+
+// pc.addEventListener('track', async (event) => {
+//     let remoteStr = remoteStream.addTrack(event.track, remoteStream);
+//     const remoteVideo = document.querySelector('#video-remote');
+//     remoteVideo.srcObject = remoteStr;
+// });
+
+// // navigator.getUserMedia({video: true, audio: true}, function (stream) {
+// //     var video = document.querySelector('video');
+// //     video.srcObject = stream;
+// //     pc.addStream(stream);
+// // }, error);
+
+
+// socket.on('add-users', function (data) {
+//     let userIdDiv = document.querySelector("#userId");
+//     userIdDiv.appendChild(
+//         document.createElement('ul').innerHTML = data.userId
+//     );
+//     let idBtn = document.querySelector('button');
+//     idBtn.addEventListener('click', (e)=>{
+//         let id = idBtn.innerHTML;
+//         createOffer(id);
+//     })
+//     // createOffer(data.userId);
+// });
+
+// socket.on('remove-user', function (id) {
+    
+// });
+
+
+// socket.on('offer-made', async (data) => {
+//     await pc.setRemoteDescription(data.offer);
+//     let answer = await pc.createAnswer();
+//     setLocalMediaStream();
+//     socket.emit('make-answer', {
+//         answer:answer,
+//         to:data.id
+//     });
+// });
+
+// socket.on('answer-made', async (data) => {
+//     await pc.setRemoteDescription(data.answer);
+//     setLocalMediaStream();
+// });
+
+// async function createOffer(id) {
+//     let offer = await pc.createOffer();
+//     await pc.setLocalDescription(offer);
+//     socket.emit('make-offer', {
+//         offer:offer,
+//         to:id
+//     })
+// }
+
+// function error(err) {
+//     console.warn('Error', err);
+// }

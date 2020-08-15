9a20575a6b8286af0a543db0faa83c1c12d900d7
nirmal <nirmal@flairlabs.com>
2020-08-15 11:46:10 +0530

    deploy

--- public/index.js
+++ public/index.js
@@ -15,69 +15,69 @@
 var socket = io.connect(window.location.origin); 
 
 var answersFrom = {}, offer;
-var peerConnection = window.RTCPeerConnection ||
-    window.mozRTCPeerConnection ||
-    window.webkitRTCPeerConnection ||
-    window.msRTCPeerConnection;
-
-var sessionDescription = window.RTCSessionDescription ||
-    window.mozRTCSessionDescription ||
-    window.webkitRTCSessionDescription ||
-    window.msRTCSessionDescription;
-
-    navigator.getUserMedia  = navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia;
-
-var pc = new peerConnection({
+var pc = new RTCPeerConnection({
     iceServers: [{
-        url: "stun:stun.services.mozilla.com",
-        username: "somename",
-        credential: "somecredentials"
+        url:["stun:stun.services.mozilla.com",
+            'stun:stun1.l.google.com:19302',
+            'stun:stun2.l.google.com:19302',]
     }]
 });
 
-pc.onaddstream = function (obj) {
-    var vid = document.createElement('video');
-    vid.setAttribute('class', 'video-small');
-    vid.setAttribute('autoplay', 'autoplay');
-    vid.setAttribute('id', 'video-small');
-    document.getElementById('users-container').appendChild(vid);
-    vid.srcObject = obj.stream;
-}
+let localStream = navigator.getUserMedia({video: true, audio: true})
+const localVideo = document.getElementsByClassName('video-local');
+localVideo.srcObject = remoteStream;
 
-navigator.getUserMedia({video: true, audio: true}, function (stream) {
-    var video = document.querySelector('video');
-    video.srcObject = stream;
-    pc.addStream(stream);
-}, error);
+localStream.getTracks().forEach(track => {
+    pc.addTrack(track, localStream);
+});
+// var pc = new peerConnection({
+//     iceServers: [{
+//         url:["stun:stun.services.mozilla.com",
+//             'stun:stun1.l.google.com:19302',
+//             'stun:stun2.l.google.com:19302',]
+//     }]
+// });
+
+// pc. = function (obj) {
+//     var vid = document.createElement('video');
+//     vid.setAttribute('class', 'video-small');
+//     vid.setAttribute('autoplay', 'autoplay');
+//     vid.setAttribute('id', 'video-small');
+//     document.getElementById('users-container').appendChild(vid);
+//     vid.srcObject = obj.stream;
+// }
+
+const remoteStream = MediaStream();
+const remoteVideo = document.getElementsByClassName('video-remote');
+remoteVideo.srcObject = remoteStream;
+
+pc.addEventListener('track', async (event) => {
+    remoteStream.addTrack(event.track, remoteStream);
+});
+
+// navigator.getUserMedia({video: true, audio: true}, function (stream) {
+//     var video = document.querySelector('video');
+//     video.srcObject = stream;
+//     pc.addStream(stream);
+// }, error);
 
 
 socket.on('add-users', function (data) {
     for (var i = 0; i < data.users.length; i++) {
-        var el = document.createElement('div'),
             id = data.users[i];
-
-        el.setAttribute('id', id);
-        el.innerHTML = id;
-        el.addEventListener('click', function () {
             createOffer(id);
-        });
-        document.getElementById('users').appendChild(el);
     }
 });
 
 socket.on('remove-user', function (id) {
-    var div = document.getElementById(id);
-    document.getElementById('users').removeChild(div);
+    
 });
 
 
 socket.on('offer-made', function (data) {
     offer = data.offer;
 
-    pc.setRemoteDescription(new sessionDescription(data.offer), function () {
+    pc.setRemoteDescription(data.offer , function () {
         pc.createAnswer(function (answer) {
             pc.setLocalDescription(new sessionDescription(answer), function () {
                 socket.emit('make-answer', {
@@ -91,8 +91,8 @@ socket.on('offer-made', function (data) {
 });
 
 socket.on('answer-made', function (data) {
-    pc.setRemoteDescription(new sessionDescription(data.answer), function () {
-        document.getElementById(data.socket).setAttribute('class', 'active');
+    pc.setRemoteDescription(data.answer, function () {
+        // document.getElementById(data.socket).setAttribute('class', 'active');
         if (!answersFrom[data.socket]) {
             createOffer(data.socket);
             answersFrom[data.socket] = true;
@@ -102,7 +102,7 @@ socket.on('answer-made', function (data) {
 
 function createOffer(id) {
     pc.createOffer(function (offer) {
-        pc.setLocalDescription(new sessionDescription(offer), function () {
+        pc.setLocalDescription(offer, function () {
             socket.emit('make-offer', {
                 offer: offer,
                 to: id
